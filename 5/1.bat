53db6e72ca1c0ed7c7abed8a58b32e55327ce2cf
Leviafan <leviafan89@mail.ru>
2011-01-30 00:05:36 +0500

    Semi-final ver. of 5 less

---
+++ 5/1.bat
@@ -0,0 +1,1 @@
+arrays.exe >1.txt
--- 5/arrays.c
+++ 5/arrays.c
@@ -1,26 +1,42 @@
-#include stdio.h
-#include time.h
-
+#include <stdio.h>
+#include <time.h>
 int main(){
-    FILE *out = fopen(result.txt,"w");
-    int Arr[2000][2000],i,j;
-    clock_t start,mid-end,final;
+    FILE *out = fopen("result.txt","w");
+    if (!out){
+        return 1; //can't open result file for write          
+    }
+    int Arra[7000][70],i,j,l;
+    clock_t start,mid_end,final;
+
+    for (i=0;i<70;i++){
+        for (j=0;j<7000;j++){
+            Arra[j][i]=0;
+        }
+    }
+
     start = clock();
-    
-    for (i=0;i<2000;i++){
-        for (j=0;j<2000;j++){
-            Arr[j][i]=0;
+    for (l=0;l<50;l++){
+        for (i=0;i<7000;i++){
+            for (j=0;j<70;j++){
+                Arra[i][j]++;
+//                printf("%i\n",Arra[i][j]);
+            }
         }
     }
-    mid-end = clock();
+
+    mid_end = clock();
     
-    for (i=0;i<2000;i++){
-        for (j=0;j<2000;j++){
-            Arr[i][j]=1;
+    for (l=0;l<50;l++){
+        for (i=0;i<70;i++){
+            for (j=0;j<7000;j++){
+                Arra[j][i]++;
+//                printf("%i\n",Arra[j][i]);
+            }
         }
     }
+
     final = clock();
-    fprintf(out,"1st method: %f.2\n", (float)(mid-end - start));
-    fprintf(out,"2nd method: %f.2\n", (float)(final - mid-end));
+    fprintf(out,"1st method: %.2f\n", (float)(mid_end - start));
+    fprintf(out,"2nd method: %.2f\n", (float)(final - mid_end));
     return 0;
 }
