cd1a13bb8bba0584e5fa425e2582cac2bb14b907
Leviafan <leviafan89@mail.ru>
2011-01-29 18:05:13 +0500

    may be it works...

--- 4/prot.asm
+++ 4/prot.asm
@@ -22,7 +22,10 @@ int 10h
 cli
 hlt
 
-errmess db "Can't read data from diskette"
+errmess db "Can't read data from diskette",0
+protected db "Hello! I am #GP!",0
+other db "Hello! I am not #GP (",0
+
 TIMES 510 - ($ - $$) db 0
 db 55h,0aah
 _2ndseg:
@@ -49,8 +52,32 @@ tend:
 xor ax, ax
 mov ds, ax
 mov ss, ax
-mov es, ax
+mov di, ax
 sti
 
 LLDT [CODE_DESCR] ;GP, here we go! 
 
+cli
+hlt
+
+other_int:
+	mov si, other
+	mov cx,	22
+	jmp cont
+int_GP:
+	mov si, protected
+	mov cx, 17
+cont:
+	mov ax, 00010000b
+	mov es, ax
+	mov	al, 07h
+print:
+	movsb
+	stosb
+	loop	print
+
+IDT:
+  times 13  dw other_int, 8, 1000011000000000b, 0
+            dw int_GP, 8, 1000011000000000b, 0
+  times 242 dw other_int, 8, 1000011000000000b, 0
+ 
