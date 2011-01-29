76c5f66f2861a73930af17ea74b3e6cd2d326d10
Leviafan <leviafan89@mail.ru>
2011-01-29 16:32:56 +0500

    4th lesson, formed GDT.4th lesson, formed GDT.4th lesson, formed GDT.4th lesson, formed GDT.

--- 4/prot.asm
+++ 4/prot.asm
@@ -1,7 +1,7 @@
 org 7c00h
 xor ax,ax
 mov es,ax
-mov ax,0201h
+mov ax,0202h
 mov cx,0002h
 mov dx,0
 mov bx,_2ndseg
@@ -12,18 +12,25 @@ mov bx,1
 
 jc cant_read
 
-jmp fine_readed
+jmp _2ndseg
 cant_read:
 mov bp,errmess
 mov cx,32
 mov ah,13h
 mov dx,0
 int 10h
-fine_readed:
-
 cli
 hlt
+
 errmess db "Can't read data from diskette"
 TIMES 510 - ($ - $$) db 0
 db 55h,0aah
 _2ndseg:
+cli
+	
+
+GDT:
+NULL_DESCR	TIMES 8 db 0
+CODE_DESCR  0FFh, 0FFh, 00h, 00h, 00h, 9Ah, 00h, 00h
+VIDEO_DESCR  00h,  00h, 00h, 80h, 0Bh, 02h, 00h, 00h
+
