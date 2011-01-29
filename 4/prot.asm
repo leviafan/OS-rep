ee5bc377d848272f4841016c5bcc4de8f80856ae
Leviafan <leviafan89@mail.ru>
2011-01-29 17:34:04 +0500

    In the middle of process...

--- 4/prot.asm
+++ 4/prot.asm
@@ -27,10 +27,30 @@ TIMES 510 - ($ - $$) db 0
 db 55h,0aah
 _2ndseg:
 cli
-	
+LGDT [GDTR]
+LIDT [IDTR]
 
+mov eax, cr0
+or al, 1
+mov cr0, eax
+
+jmp 8:tend
 GDT:
 NULL_DESCR	TIMES 8 db 0
 CODE_DESCR  0FFh, 0FFh, 00h, 00h, 00h, 9Ah, 00h, 00h
-VIDEO_DESCR  00h,  00h, 00h, 80h, 0Bh, 02h, 00h, 00h
+VIDEO_DESCR  00h,  10h, 00h, 80h, 0Bh, 02h, 00h, 00h
+tsize dw $-GDT
+GDTR    dw tsize
+        dd GDT
+IDTR    dw 800h
+        dd IDT
+
+tend:
+xor ax, ax
+mov ds, ax
+mov ss, ax
+mov es, ax
+sti
+
+LLDT [CODE_DESCR] ;GP, here we go! 
 
