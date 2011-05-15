4ebf466a5716667bbb03c621623d1909f7b9184e
Leviafan <leviafan89@mail.ru>
2011-05-16 01:37:42 +0600

    Task 2 new revision.

--- 2/2.c
+++ 2/2.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]){
     int i, check, *tmp_heap, *heap_with_numbers=NULL, error_code=0, unit=0, unit_number=0, file_cntr=1, argum=argc;
     char dummy, str[50];
     if (argc<2){
-        if (!show_mess("\nToo few arguments. I need one file at least")) error_code=1;
+        if (show_mess("\nToo few arguments. I need one file at least")>0) error_code=1;
         return error_code;
     }
     do{
@@ -26,10 +26,10 @@ int main(int argc, char *argv[]){
             return error_code;
         }
         while (!feof(r_file)){
-            if (!fscanf (r_file,"%i ",&unit)){
+            if (fscanf (r_file,"%i ",&unit)==1){
                 strcpy (str,"\nIncorrect data in file: ");
-                if (!show_mess(strcat(str,argv[file_cntr]))) error_code=1;
-                while (!fscanf (r_file,"%i ",&unit)){fscanf (r_file,"%s",&dummy);}
+                if (show_mess(strcat(str,argv[file_cntr]))>0) error_code=1;
+                while (fscanf (r_file,"%i ",&unit)==1){fscanf (r_file,"%s",&dummy);}
             }
             tmp_heap = (int*) realloc (heap_with_numbers, (unit_number+1)*sizeof(int));
   			if (tmp_heap){
@@ -39,7 +39,7 @@ int main(int argc, char *argv[]){
             else{
                 free (heap_with_numbers);
                 fclose(r_file);
-                if (!show_mess("\nCan't realloc memory. ")) error_code=1;
+                if (show_mess("\nCan't realloc memory. ")>0) error_code=1;
                 return error_code;
             }
             unit_number++;
@@ -52,7 +52,7 @@ int main(int argc, char *argv[]){
     r_file=fopen(argv[argc-1],"w");
     if (!r_file){
         strcpy (str,"\nCan't open for write file: ");
-        if (!show_mess(strcat(str,argv[argc-1]))) error_code=1;
+        if (show_mess(strcat(str,argv[argc-1]))>0) error_code=1;
         free(heap_with_numbers);
         return error_code; 
     }
@@ -61,7 +61,7 @@ int main(int argc, char *argv[]){
         if (!fprintf (r_file,"%i ",heap_with_numbers[i])){
             fclose(r_file);
             strcpy (str,"\nCan't write data into file: ");
-            if (!(show_mess(strcat(str,argv[argc-1])))) error_code=1;
+            if (show_mess(strcat(str,argv[argc-1]))>0) error_code=1;
         }
     }
     fclose(r_file);
